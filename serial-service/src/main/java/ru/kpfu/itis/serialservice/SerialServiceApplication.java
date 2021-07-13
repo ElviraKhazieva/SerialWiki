f4f02ffd59292799fb45270bb3d9df8c86daf9dd
Anatoly <antonov250901@mail.ru>
2021-07-13 15:11:26 +0300

    init

---
+++ serial-service/src/main/java/ru/kpfu/itis/serialservice/SerialServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.kpfu.itis.serialservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerialServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerialServiceApplication.class, args);
+    }
+
+}
