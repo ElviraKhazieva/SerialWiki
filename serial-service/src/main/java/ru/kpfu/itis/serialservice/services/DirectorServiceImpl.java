29fdedb92bd211f1e36c3c3d089a70272499e21a
Anatoly <antonov250901@mail.ru>
2021-07-16 14:03:23 +0300

    moved services

--- serial-service/src/main/java/ru/kpfu/itis/serialservice/services/DirectorServiceImpl.java
+++ serial-service/src/main/java/ru/kpfu/itis/serialservice/services/DirectorServiceImpl.java
@@ -1,9 +1,10 @@
-package ru.kpfu.itis.serialservice.api.services;
+package ru.kpfu.itis.serialservice.services;
 
 import org.springframework.stereotype.Service;
 import ru.kpfu.itis.serialservice.api.dto.DirectorDto;
 import ru.kpfu.itis.serialservice.api.dto.DirectorForm;
 import ru.kpfu.itis.serialservice.api.dto.GenreDto;
+import ru.kpfu.itis.serialservice.api.services.DirectorService;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,11 +24,11 @@ public class DirectorServiceImpl implements DirectorService {
 
     @Override
     public List<DirectorDto> getAllDirector() {
-        return Arrays.asList(new DirectorDto[]{DirectorDto.builder().
+        return Arrays.asList(DirectorDto.builder().
                 id(1L)
                 .name("dir_fir").build(),
                 DirectorDto.builder().
                         id(2L)
-                        .name("dirsec").build()});
+                        .name("dirsec").build());
     }
 }
--- serial-service/src/main/java/ru/kpfu/itis/serialservice/services/GenreServiceImpl.java
+++ serial-service/src/main/java/ru/kpfu/itis/serialservice/services/GenreServiceImpl.java
@@ -1,7 +1,8 @@
-package ru.kpfu.itis.serialservice.api.services;
+package ru.kpfu.itis.serialservice.services;
 
 import org.springframework.stereotype.Service;
 import ru.kpfu.itis.serialservice.api.dto.GenreDto;
+import ru.kpfu.itis.serialservice.api.services.GenreService;
 import ru.kpfu.itis.serialservice.models.Genre;
 
 import java.util.Arrays;
@@ -18,13 +19,13 @@ public class GenreServiceImpl implements GenreService {
     @Override
     public List<GenreDto> getAllGenre() {
 
-        return Arrays.asList(new GenreDto[]{GenreDto.builder().
+        return Arrays.asList(GenreDto.builder().
                 id(1L).
                 description("blabla")
                 .name("fir").build(),
                 GenreDto.builder().
                         id(2L).
                         description("22blabla")
-                        .name("sec").build()});
+                        .name("sec").build());
     }
 }
