299f137e8b06c9a1098617dd8f0cef4451b494c5
Anatoly <antonov250901@mail.ru>
2021-07-16 17:05:15 +0300

    added SerialRestController

--- serial-service/src/main/java/ru/kpfu/itis/serialservice/controllers/SerialController.java
+++ serial-service/src/main/java/ru/kpfu/itis/serialservice/controllers/SerialController.java
@@ -4,21 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.kpfu.itis.serialservice.api.dto.SerialForm;
 import ru.kpfu.itis.serialservice.api.services.DirectorService;
 import ru.kpfu.itis.serialservice.api.services.GenreService;
+import ru.kpfu.itis.serialservice.api.services.SerialService;
 
 @Controller
 public class SerialController {
     @Autowired
-    DirectorService directorService;
+    private DirectorService directorService;
     @Autowired
-    GenreService genreService;
+    private GenreService genreService;
+    @Autowired
+    private SerialService serialService;
 
-    @GetMapping("adding_serial")
+    @GetMapping("/adding_serial")
     public String getAddingPage(Model model){
         model.addAttribute("directors",directorService.getAllDirector());
         model.addAttribute("genres",genreService.getAllGenre());
         return "add_serial_page";
 
+    }
+
+    @PostMapping("/create")
+    public String addSerial(SerialForm form){
+        serialService.addSerial(form);
+        return "redirect:/serial_view";
     }
 }
